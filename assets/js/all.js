b581c79e2ad52b55c61a2ed4b9330d8668dfe575
Phoebe <xqrass@gmail.com>
2022-09-14 23:24:45 +0800

    Update 2022-09-14T15:24:31.226Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,66 +1,57 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   // ham
-  $('.ham-show').click(function (e) { 
+  $('.ham-show').click(function (e) {
     e.preventDefault();
     $('.ham-dropDown').slideToggle('show');
-  })
+  }); // question
 
-  // question
   $('.question').click(function (e) {
     // e.preventDefault();
     $(this).parent('.question-content').find('.answer').toggleClass('active');
     $(this).parent('.question-content').find('i').toggleClass('fa-caret-down fa-caret-up');
-  })
+  }); //  select
 
-  //  select
   $('.select-all').click(function (e) {
     $('.select-all').addClass('active');
     $('.productsList-all').addClass('active');
-
     $('.select-plants').removeClass('active');
     $('.productsList-plants').removeClass('active');
-
     $('.select-space').removeClass('active');
     $('.productsList-space').removeClass('active');
-  })
-
+  });
   $('.select-plants').click(function (e) {
     $('.select-plants').addClass('active');
     $('.productsList-plants').addClass('active');
-
     $('.select-all').removeClass('active');
     $('.productsList-all').removeClass('active');
-
     $('.select-space').removeClass('active');
     $('.productsList-space').removeClass('active');
-  })
-
+  });
   $('.select-space').click(function (e) {
     $('.select-space').addClass('active');
     $('.productsList-space').addClass('active');
-
     $('.select-all').removeClass('active');
     $('.productsList-all').removeClass('active');
-
     $('.select-plants').removeClass('active');
     $('.productsList-plants').removeClass('active');
-  })
+  });
+}); // Swiper
 
-});
-
-// Swiper
 var swiper = new Swiper(".mySwiper", {
   on: {
-    init: function () {
+    init: function init() {
       console.log('swiper initialized');
-    },
+    }
   },
   loop: true,
   autoplay: {
-    delay: 8000,
+    delay: 8000
   },
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    prevEl: ".swiper-button-prev"
+  }
 });
+//# sourceMappingURL=all.js.map
